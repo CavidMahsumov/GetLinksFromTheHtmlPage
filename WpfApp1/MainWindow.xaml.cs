5f4143064e8f5861b06753523eb992b4d6edcb7a
Mahs_kz07 <Mahs_kz07@STHQ0124-16>
2021-12-18 16:27:26 +0400

    Add project files.

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string urlAddress = AdressName.Text;
+
+            //HttpWebRequest request = (HttpWebRequest)WebRequest.Create(urlAddress);
+            //HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            //if (response.StatusCode == HttpStatusCode.OK)
+            //{
+            //    Stream receiveStream = response.GetResponseStream();
+            //    StreamReader readStream = null;
+            //    if (String.IsNullOrWhiteSpace(response.CharacterSet))
+            //        readStream = new StreamReader(receiveStream);
+            //    else
+            //        readStream = new StreamReader(receiveStream,
+            //            Encoding.GetEncoding(response.CharacterSet));
+            //    string data = readStream.ReadToEnd();
+            //    response.Close();
+            //    readStream.Close();
+
+            //    Console.WriteLine(data);
+
+
+            
+                var doc = new HtmlWeb().Load(urlAddress);
+                var linkTags = doc.DocumentNode.Descendants("link");
+                var linkedPages = doc.DocumentNode.Descendants("a")
+                                                  .Select(a => a.GetAttributeValue("href", null))
+                                                  .Where(u => !String.IsNullOrEmpty(u));
+
+
+
+
+
+            Main.ItemsSource = linkedPages;
+            
+           
+            
+        }
+    }
+}
