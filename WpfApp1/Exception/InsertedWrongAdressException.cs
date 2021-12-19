8f2f80a272ef7dffcca75f3ae66f1abd7cc2c23d
Mahs_kz07@itstep.edu.az <Mahs_kz07@itstep.edu.az>
2021-12-19 13:14:50 +0400

    123

---
+++ WpfApp1/Exception/InsertedWrongAdressException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1.Exception1
+{
+    public class InsertedWrongAdressException:Exception
+    {
+        public InsertedWrongAdressException(string message) : base(message)
+        {
+
+        }
+    }
+}
--- WpfApp1/ViewModel/MainWindowViewModel.cs
+++ WpfApp1/ViewModel/MainWindowViewModel.cs
@@ -6,51 +6,72 @@ using System.Linq;
 using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using WpfApp1.Commands;
+using WpfApp1.Exception1;
 
 namespace WpfApp1.ViewModel
 {
-   public class MainWindowViewModel:BaseViewModel
+    public class MainWindowViewModel : BaseViewModel
     {
         public string urlAddress { get; set; }
 
         public RelayCommand SearchBtnClick { get; set; }
         public MainWindowViewModel(MainWindow mainWindow)
         {
-            SearchBtnClick = new RelayCommand((sender) => {
-
-                urlAddress = mainWindow.AdressName.Text;
-                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(urlAddress);
-                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
-                if (response.StatusCode == HttpStatusCode.OK)
+            SearchBtnClick = new RelayCommand((sender) =>
+            {
+               
+                try
                 {
-                    Stream receiveStream = response.GetResponseStream();
+                    urlAddress = mainWindow.AdressName.Text;
+                    HttpWebRequest request = (HttpWebRequest)WebRequest.Create(urlAddress);
+                    HttpWebResponse response = (HttpWebResponse)request.GetResponse();
                     StreamReader readStream = null;
-                    if (String.IsNullOrWhiteSpace(response.CharacterSet))
-                        readStream = new StreamReader(receiveStream);
-                    else
-                        readStream = new StreamReader(receiveStream,
 
-                            Encoding.GetEncoding(response.CharacterSet));
+                    if (response.StatusCode == HttpStatusCode.OK)
+                    {
+                        Stream receiveStream = response.GetResponseStream();
 
-                    string data = readStream.ReadToEnd();
-                    var doc = new HtmlWeb().Load(urlAddress);
-                    var linkTags = doc.DocumentNode.Descendants("link");
-                    var linkedPages = doc.DocumentNode.Descendants("a")
-                                                      .Select(a => a.GetAttributeValue("href", null))
-                                                      .Where(u => !String.IsNullOrEmpty(u));
-                    string[] arr = linkedPages.ToArray();
-                    mainWindow.Adress.Header = mainWindow.AdressName.Text;
-                    mainWindow.one.Header = arr[0];
-                    mainWindow.two.Header = arr[1];
-                    mainWindow.three.Header = arr[2];
-                    mainWindow.four.Header = arr[3];
-                    mainWindow.five.Header = arr[4];
-                    mainWindow.six.Header = arr[5];
-                    mainWindow.Seven.Header = arr[6];
-                    response.Close();
-                    readStream.Close();
+                        if (String.IsNullOrWhiteSpace(response.CharacterSet))
+                            readStream = new StreamReader(receiveStream);
+                        else
+                            readStream = new StreamReader(receiveStream,
+
+                                Encoding.GetEncoding(response.CharacterSet));
+
+                        string data = readStream.ReadToEnd();
+                        var doc = new HtmlWeb().Load(urlAddress);
+                        var linkTags = doc.DocumentNode.Descendants("link");
+                        var linkedPages = doc.DocumentNode.Descendants("a")
+                                                          .Select(a => a.GetAttributeValue("href", null))
+                                                          .Where(u => !String.IsNullOrEmpty(u));
+                        string[] arr = linkedPages.ToArray();
+                        if (arr != null)
+                        {
+                            mainWindow.Adress.Header = mainWindow.AdressName.Text;
+                            mainWindow.one.Header = arr[0];
+                            mainWindow.two.Header = arr[1];
+                            mainWindow.three.Header = arr[2];
+                            mainWindow.four.Header = arr[3];
+                            mainWindow.five.Header = arr[4];
+                            mainWindow.six.Header = arr[5];
+                            mainWindow.Seven.Header = arr[6];
+                        }
+                        else
+                        {
+                            throw new InsertedWrongAdressException("Adress Not Found! Try Again");
+                        }
+
+                    }
                 }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                
+
+
 
             });
 
